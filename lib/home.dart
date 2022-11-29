757d1b232594fe366d26062448277eeed993083f
ajmlmohmed <ajmalashraf983@gmail.com>
2022-11-29 14:42:47 +0530

    seminar content

---
+++ lib/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+
+import 'liquid.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    final pages = [
+      const LiquidContiners(
+        colors: Color.fromARGB(255, 150, 208, 255),
+        images: 'assets/audience.png',
+        title: 'Travel',
+        buttontext: 'Skip',
+      ),
+      const LiquidContiners(
+        colors: Color.fromARGB(255, 166, 241, 250),
+        images: 'assets/audience (1).png',
+        title: 'Tourism',
+        buttontext: 'Skip',
+      ),
+       const LiquidContiners(
+        colors: Color.fromARGB(255, 234, 120, 120),
+        images: 'assets/director.png',
+        title: 'Trip',
+        buttontext: 'Finish',
+      ),
+    ];
+    return Scaffold(
+        body: LiquidSwipe(
+      pages: pages,
+      positionSlideIcon: 0.8,
+      waveType: WaveType.liquidReveal,
+      enableSideReveal: true,
+      fullTransitionValue: 1000,
+      slideIconWidget: const Icon(Icons.arrow_left_outlined),
+    ));
+  }
+}
